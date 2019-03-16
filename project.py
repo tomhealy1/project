d6ac0b0856078990025a5c30f14462ebe9c3145e
tomhealy1 <alfaone156@gmail.com>
2019-03-16 21:15:05 +0000

    Messed with a Jupyter Notebook

--- project.py
+++ project.py
@@ -1,8 +1,15 @@
 #HDip in Computer Science (Data Analysis)
 #Tom Healy
+#Import packages needed
 import pandas as pd
-import numpy as nd
-
+import numpy as np
+#Assign csv file to df
 df = pd.read_csv("iris_csv.csv")
+#Print the columns names
+print(df.columns)
 
-print(df)
+#print the descriptive stats
+y = df.describe()
+
+print(y)
+
