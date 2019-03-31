e6d94d18692a3727f1d284e716bc72b5e4f0f4b4
tomhealy1 <alfaone156@gmail.com>
2019-03-31 13:17:05 +0100

    Added 3d scatterplot

--- boxwhisker.py
+++ boxwhisker.py
@@ -9,6 +9,7 @@ iris = "iris.txt"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 df = pandas.read_csv(iris, names=names)
 
-# 
+# Command to plot and show the boxwhisker plot
 df.plot(kind='box', subplots=True, layout=(2,2), sharex=False, sharey=False)
 plt.show()
+
