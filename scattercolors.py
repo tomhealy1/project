f4c2199b4994a9aff2e5e053218ef020829b3b3f
tomhealy1 <alfaone156@gmail.com>
2019-04-24 07:54:24 +0100

    Added comments

--- scattercolors.py
+++ scattercolors.py
@@ -1,11 +1,12 @@
 #https://pandas.pydata.org/pandas-docs/stable/user_guide/visualization.html#basic-plotting-plot
+#Import packages as needed
 import pandas as pd 
 import numpy as np
 import matplotlib.pyplot as plt
-
+#Load the data as tetx separated by a "," and the columns we will be using are the 1st, 2nd, 3rd and 4th. Assign the data to the var "iris"
 iris = np.loadtxt('iris.txt', delimiter=',', usecols=[0,1,2,3])
 
-
+#We create a dataframe from the data used in the iris var.
 df = pd.DataFrame(iris, columns=['a', 'b', 'c', 'd'])
 ax = df.plot.scatter(x='a', y='b', s=50,color='DarkBlue', label='Sepal')
 df.plot.scatter(x='c', y='d', s=50, color='Red', label='Petal', ax=ax)
