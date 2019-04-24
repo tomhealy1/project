0ffc84a080253c975b5b7573ed7a4f01e28c690d
tomhealy1 <alfaone156@gmail.com>
2019-04-24 07:47:18 +0100

    Added comments

--- 3dscatter.py
+++ 3dscatter.py
@@ -1,16 +1,17 @@
 # Tom Healy 
 #https://stackoverflow.com/questions/43484757/creating-a-3d-scatter-plot-from-a-csv-file
 #https://matplotlib.org/gallery/mplot3d/2dcollections3d.html
-
+#Import the packages as needed 
 import matplotlib.pyplot as plt
 import numpy as np 
 from mpl_toolkits.mplot3d import Axes3D
-
+#
 fig = plt.figure()
 ax1 = fig.add_subplot(111, projection='3d')
-#I am trying add color to each different column but cant find a way yet
 a, b, c, d = np.loadtxt('iris.txt', delimiter=',',dtype=float, unpack=True, usecols=[0,1 ,2, 3])
 
 
 ax1.scatter(a, b,c, d, c='blue', marker='o')
 plt.show()
+
+#The next step here would have been to add colors to the 3d pic but I supoose this is to give an overall impression of the data on a 3d scale.
