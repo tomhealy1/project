a357841593d93295ba34c3e3054400463cb06dbb
tomhealy1 <alfaone156@gmail.com>
2019-04-27 12:21:12 +0100

    Added final comments

--- 3dscatter.py
+++ 3dscatter.py
@@ -5,13 +5,14 @@
 import matplotlib.pyplot as plt
 import numpy as np 
 from mpl_toolkits.mplot3d import Axes3D
-#
+
+#We plot the figure using the 3D package, add the projection, color and the data. 
 fig = plt.figure()
 ax1 = fig.add_subplot(111, projection='3d')
 a, b, c, d = np.loadtxt('iris.txt', delimiter=',',dtype=float, unpack=True, usecols=[0,1 ,2, 3])
 
-
+#Create the 2d scatter based on the columns using the 'o' as a marker for each data point 
 ax1.scatter(a, b,c, d, c='blue', marker='o')
 plt.show()
 
-#The next step here would have been to add colors to the 3d pic but I supoose this is to give an overall impression of the data on a 3d scale.
+#The next step here would have been to add colors to the 3d pic but I suppose this is adequate to give an overall impression of the data on a 3d scale.
