72fb8456c7fbe5255f338a2b465216ff2e0fe500
tomhealy1 <alfaone156@gmail.com>
2019-04-27 12:50:13 +0100

    Added final comments

--- pca.py
+++ pca.py
@@ -1,34 +1,36 @@
+#Tom Healy
 #http://stamfordresearch.com/pca-in-python-with-scikit-learn/
+#Import pandas, numpy and matplotlib
 import numpy as np
 import pandas as pd
- 
 import matplotlib
 import matplotlib.pyplot as plt
- 
- 
+
+#Import the sklearn package 
 from sklearn.decomposition import PCA
 from sklearn import datasets
 
-# Pandas Options (optional)
+#Tell pandas to not display too much
 pd_maxrows = 10
 pd.set_option("display.max_rows",pd_maxrows)
- 
 pd.set_option("display.max_columns",30)
 
+#Assign the oloaded dataset to the variable iris. X = inputs and y = outputs
 iris = datasets.load_iris()
 X = iris.data
 y = iris.target
 
-print(iris.feature_names)
+
+print("Here are the inputs", iris.feature_names)
  
-print(iris.target_names)
+print("Here are the outputs", iris.target_names)
 
 
 
 df = pd.DataFrame(X)
 df.columns = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
  
-# Add the class names
+#Add the class names or output names
 df['labels'] = iris.target_names[y]
  
 print(df)
@@ -36,7 +38,7 @@ print(df)
 	
 labels = df['labels'].unique().tolist()
 
-
+#Here we are plotting the Sepal measure(LHS Chart) and Petal (RHS Chart)
 plt.figure(figsize=(15,5))
 plt.subplot(121)
 for lab in labels:
@@ -50,17 +52,22 @@ for lab in labels:
 
 plt.show()
 
-
+#This is the PCA part of the script
+#Creates an array
 X = df.iloc[:,0:4]
 
+#Fit the PCA
 pca = PCA(n_components=2)
 pca.fit(X)
 X_ = pca.transform(X)
 
+#Create a dataframe called dfPCA
 dfPCA = pd.DataFrame({'x1': X_[:,0], 'x2': X_[:,1]})
 dfPCA['labels'] = df['labels']
 dfPCA
+print(dfPCA)
 
+#We then plot the PCA
 plt.figure(figsize=(7,5))
 for lab in labels:
     plt.scatter(dfPCA.loc[dfPCA['labels'] == lab, 'x1'],  dfPCA.loc[dfPCA['labels'] == lab, 'x2'], label=lab)
