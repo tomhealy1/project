8e935bd99aa005d692320ecf310b77a464f8186d
tomhealy1 <alfaone156@gmail.com>
2019-04-27 10:44:56 +0100

    added to readme

--- scattermatrix.py
+++ scattermatrix.py
@@ -1,13 +1,21 @@
-import pandas
-from pandas.plotting import scatter_matrix
-import matplotlib.pyplot as plt
+#import pandas
+#from pandas.plotting import scatter_matrix
+#import matplotlib.pyplot as plt
 
 # Assign the file to var iris, assign names to the cols and pass the panda read file to df
-iris = "iris.txt"
-names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
-df = pandas.read_csv(iris, names=names)
+#iris = "iris.txt"
+#names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
+#df = pandas.read_csv(iris, names=names)
 
 #Call on the scatter_matrix mod in pandas to create a simple scatter matrix 
-scatter_matrix(df, figsize=None,diagonal='hist')
-plt.show()
+#scatter_matrix(df, figsize=None,color=colors,diagonal='hist')
+#plt.show()
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set(style="ticks", color_codes=True)
+iris = sns.load_dataset("iris")
 
+iris = sns.load_dataset("iris")
+sns.pairplot(iris, hue="species")
+
+plt.show()
