2142c2e74c0f88ef132f477866e3f9df28c57bcd
tomhealy1 <alfaone156@gmail.com>
2019-04-27 12:54:25 +0100

    Added final comments

--- radviz.py
+++ radviz.py
@@ -1,3 +1,5 @@
+#Tom Healy
+#This graph works on the idea that each point is "connected to a spring from the centre" . Shows the level of correlation also. Pretty cool graph.
 import pandas as pd 
 from pandas.plotting import radviz
 import matplotlib.pyplot as plt
